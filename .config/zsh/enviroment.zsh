12a81535397b3373b5f1946a9406f891a6cffa08
random2907 <yarnjeetyadav1234567890@gmail.com>
2025-05-27 14:14:27 +0530

    zsh and trash

--- .config/zsh/enviroment.zsh
+++ .config/zsh/enviroment.zsh
@@ -1,8 +1,14 @@
 #path
 export PATH=$XDG_DATA_HOME/flatpak/exports/bin:$PATH
+export PYTHONSTARTUP=$XDG_CONFIG_HOME/python/pythonrc
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$HOME/development/flutter/bin:$PATH
 export PATH=$HOME/development/cmdline-tools/bin:$PATH
+export PATH="/home/ranjeet/perl5/bin${PATH:+:${PATH}}"
+export PERL5LIB="/home/ranjeet/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"
+export PERL_LOCAL_LIB_ROOT="/home/ranjeet/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"
+export PERL_MB_OPT="--install_base \"/home/ranjeet/perl5\""
+export PERL_MM_OPT="INSTALL_BASE=/home/ranjeet/perl5"
 export MANPAGER='nvim +Man!'
 export CUDA_CACHE_PATH="$XDG_CACHE_HOME"/nv
 export ANDROID_USER_HOME="$XDG_DATA_HOME"/android
